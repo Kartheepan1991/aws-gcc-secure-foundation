1203b4fe411dd18de371ea7bc12fb21f6c17ba95
Kartheepan1991 <rkartheepan374@gmail.com>
2026-02-01 11:17:24 +0530

    Fix node group recreation: use name_prefix for create_before_destroy compatibility

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -60,7 +60,7 @@ resource "aws_iam_openid_connect_provider" "cluster" {
 # EKS Managed Node Group
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.environment}-node-group"
+  node_group_name_prefix = "${var.environment}-node-group-"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.private_subnet_ids
   version         = var.cluster_version
